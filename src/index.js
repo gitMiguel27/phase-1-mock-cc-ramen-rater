4d0fa1f3323088190860364357a80e82fba2a4de
Miguel Nazario <mnazario35@gmail.com>
2022-02-10 21:50:34 -0500

    commit tree a3f667a8871c01b0c5e1ed7148394ac0c31e4911

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-// write your code here
+let ramenMenuDiv = document.getElementById('ramen-menu');
+let ramenDetailDiv = document.getElementById('ramen-detail');
+let ramenDetailImage = document.querySelector('.detail-image');
+let ramenDetailName = document.querySelector('.name');
+let ramenDetailRestaurant = document.querySelector('.restaurant');
+// console.log(ramenDetailDiv, ramenDetailImage, ramenDetailName, ramenDetailRestaurant);
+let ramenRatingDisplay = document.getElementById('rating-display');
+let ramenCommentDisplay = document.getElementById('comment-display');
+
+let newRamenForm = document.getElementById('new-ramen');
+let newName = document.getElementById('new-name');
+let newRestaurant = document.getElementById('new-restaurant');
+let newImage = document.getElementById('new-image');
+let newRating = document.getElementById('new-rating');
+let newComment = document.getElementById('new-comment');
+
+fetch("http://localhost:3000/ramens")
+.then(response => response.json())
+.then(ramenData => {
+    // console.log(ramenData);
+    ramenData.forEach(ramens => {
+        // console.log(ramenElement.name);
+        renderImages(ramens);
+    });
+
+    renderNewRamen();
+
+})
+
+function renderImages(ramens) {
+    let ramenImage = document.createElement('img');
+    ramenImage.src = ramens.image;
+
+    ramenImage.addEventListener('click', event => {
+        // console.log('clicked!');
+        ramenDetailImage.src = ramens.image;
+        ramenDetailName.textContent = ramens.name;
+        ramenDetailRestaurant.textContent = ramens.restaurant;
+        ramenRatingDisplay.textContent = ramens.rating;
+        ramenCommentDisplay.textContent = ramens.comment;
+    })
+
+    ramenMenuDiv.append(ramenImage);
+}
+
+function renderNewRamen() {
+    newRamenForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        // console.log('submitted!');
+        let newRamen = {
+            name: newName.name,
+            restaurant: newRestaurant.value,
+            image: newImage.value,
+            rating: newRating.value,
+            comment: newComment.value
+        }
+         // console.log(newRamen);
+        renderImages(newRamen);
+        ramenMenuDiv.append(newRamen);
+
+        newRamenForm.reset();
+    })
+    
+}
